8f2f83ffc6a95f5264b8d96981a773fb8ef38b01
sfeakes <sfeakes@gmail.com>
2025-04-25 17:11:42 -0500

    Updates

--- js/header.js
+++ js/header.js
@@ -83,8 +83,16 @@ header.innerHTML="<div class='flexbox'>"+
 const footer = document.getElementById('footer');
 
 footer.innerHTML="<div class='grid'>"+
+      "<div class='unit whole center-on-mobiles''>"+
+      "<p>If you like this project, please consider donating <a href=' https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=SEGN9UNS38TXJ'"+
+        "rel='nofollow'><img "+
+          "src='https://camo.githubusercontent.com/2b3b3f38604d749b543e8577afdc6bd9fab25244f6cb16bfb713273a74350fd7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446f6e6174652d50617950616c2d677265656e2e737667'"+
+          "alt='Donate' data-canonical-src='https://img.shields.io/badge/Donate-PayPal-green.svg'"+
+          "style='max-width: 100%;'></a>"+
+        "</p>"+
+      "</div>"+
       "<div class='unit two-thirds center-on-mobiles'>"+
-        "<p>AqualinkD is actively maintained in <a href='https://github.com/aqualinkd/'>core repos</a> free under the"+
+        "<p>AqualinkD is actively maintained in <a href='https://github.com/aqualinkd/'>core repos</a> free under the "+
         "terms of the <a href='https://github.com/aqualinkd/AqualinkD/blob/master/LICENSE.md'>GPLv2 licensing</a>.</p>"+
           "</div>"+
       "<div class='unit one-third align-right center-on-mobiles'>"+
@@ -92,13 +100,4 @@ footer.innerHTML="<div class='grid'>"+
         "Hosted by <a href='https://github.com'> • GitHub</a>"+
           "</p>"+
         "</div>"+
-
-      "<div class='unit whole center-on-mobiles''>"+
-      "<p>If you like this project, please consider donating <a href=' https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=SEGN9UNS38TXJ'"+
-        "rel='nofollow'><img"+
-          "src='https://camo.githubusercontent.com/2b3b3f38604d749b543e8577afdc6bd9fab25244f6cb16bfb713273a74350fd7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446f6e6174652d50617950616c2d677265656e2e737667'"+
-          "alt='Donate' data-canonical-src='https://img.shields.io/badge/Donate-PayPal-green.svg'"+
-          "style='max-width: 100%;'></a>"+
-        "</p>"+
-      "</div>"+
 "</div>";
