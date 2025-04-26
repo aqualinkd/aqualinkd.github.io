546428043201e70bf7f965de9225f402f47a7d66
sfeakes <sfeakes@gmail.com>
2025-04-25 19:33:56 -0500

    updates

---
+++ js/expandImages.js
@@ -0,0 +1,54 @@
+
+try {
+  const body = document.body;
+
+  const mainDiv = document.createElement('div');
+  mainDiv.id = "expandedImage";
+  mainDiv.classList.add("modal");
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add("modal-content");
+  contentDiv.innerHTML = "<span class=\"close\">&times;</span>"+
+                         "<img class=\"pointer\" src=\"\" id=\"modal-image\" width=\"90%\" height=\"90%\"/>";
+  
+  mainDiv.appendChild(contentDiv);             
+  body.appendChild(mainDiv);
+} catch (e){}
+
+/*
+<div id="expandedImage" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <img class="pointer" src="" id="modal-image" width="90%" height="90%"/>
+  </div>
+</div>
+*/
+
+
+//let imageBox1 = document.getElementById("imageBox1");
+//var modal = document.getElementById("expandedImage");
+
+function expandImage(e) {
+
+  var modal = document.getElementById("expandedImage");
+  modal.style.display = "block";
+
+  var src = e.src;
+  var modalImage = document.getElementById("modal-image");
+  modalImage.src = src;
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+}
+
+window.onclick = function (event) {
+  if (event.target == modal || event.target.id == "modal-image") {
+    var modal = document.getElementById("expandedImage");
+    modal.style.display = "none";
+  }
+  //console.log(event.target.id);
+  //console.log(event.target);
+  //console.log("click");
+};
--- js/header.js
+++ js/header.js
@@ -14,90 +14,89 @@ const currentPathname = window.location.pathname;
 //const pageName = currentPathname.split("/").pop();
 
 const pageName = currentPathname.split("/").slice(-2, -1).toString();
-//pageName = pageName.replace('/\//+/g', '');
-console.log("Page name:", pageName);
 
-const header = document.getElementById('header');
+let pshome = "";
+let psoverview = "";
+let psdetails = "";
+let pspurchase = "";
+let psdocs = "";
 
-let pshome="";
-let psoverview="";
-let psdetails="";
-let pspurchase="";
-let psdocs="";
-
-switch(pageName){
+switch (pageName) {
   case "overview":
-    psoverview=" class='current'";
-  break;
+    psoverview = " class='current'";
+    break;
   case "details":
-    psdetails=" class='current'";
-  break;
+    psdetails = " class='current'";
+    break;
   case "docs":
-    psdocs=" class='current'";
-  break;
+    psdocs = " class='current'";
+    break;
   case "purchase":
-    pspurchase=" class='current'";
-  break;
+    pspurchase = " class='current'";
+    break;
   case "":
   default:
-    pshome=" class='current'";
-  break;
+    pshome = " class='current'";
+    break;
 }
 
 // "<img src='/img/logo.png' width='237' height='46' alt='AqualinkD'>"+
+try {
+  const header = document.getElementById('header');
 
-header.innerHTML="<div class='flexbox'>"+
-      "<div class='center-on-mobiles'>"+
-        "<h1>"+
-        "<a href='/' class='logo'>"+
-          "<span class='sr-only'>AqualinkD</span>"+
-           
-            "AqualinkD"+
-            "</a>"+
-          "</h1>"+
-        "</div> "+
-      "<nav class='main-nav hide-on-mobiles'>"+
-      "<ul>"+
-          "<li"+pshome+"><a href='/'>Home</a></li>"+
-          "<li"+psoverview+"><a href='/overview/'>Overview</a></li>"+
-          "<li"+psdocs+"><a href='/docs/'>Docs</a></li>"+
-          "<li"+psdetails+"><a href='/details/'>Details</a></li>"+
-          "<li"+pspurchase+"><a href='/purchase/'>Purchase</a></li>"+
-          "<li><a href='https://github.com/aqualinkd' target='_blank' rel='noopener'>GitHub</a></li>"+
-          "</ul>"+
-        "</nav>"+
-      "</div>"+
+  header.innerHTML = "<div class='flexbox'>" +
+    "<div class='center-on-mobiles'>" +
+    "<h1>" +
+    "<a href='/' class='logo'>" +
+    "<span class='sr-only'>AqualinkD</span>" +
+    "AqualinkD" +
+    "</a>" +
+    "</h1>" +
+    "</div> " +
+    "<nav class='main-nav hide-on-mobiles'>" +
+    "<ul>" +
+    "<li" + pshome + "><a href='/'>Home</a></li>" +
+    "<li" + psoverview + "><a href='/overview/'>Overview</a></li>" +
+    "<li" + psdocs + "><a href='/docs/'>Docs</a></li>" +
+    "<li" + psdetails + "><a href='/details/'>Details</a></li>" +
+    "<li" + pspurchase + "><a href='/purchase/'>Purchase</a></li>" +
+    "<li><a href='https://github.com/aqualinkd' target='_blank' rel='noopener'>GitHub</a></li>" +
+    "</ul>" +
+    "</nav>" +
+    "</div>" +
 
-    "<nav class='mobile-nav show-on-mobiles'>"+
-      "<ul>"+
-        "<li"+pshome+"><a href='/'>Home</a></li>"+
-        "<li"+psoverview+"><a href='/overview/'>Overview</a></li>"+
-        "<li"+psdocs+"><a href='/docs/'>Docs</a></li>"+
-        "<li"+psdetails+"><a href='/details/'>Details</a></li>"+
-        "<li"+pspurchase+"><a href='/purchase/'>Purchase</a></li>"+
-        "<li><a href='https://github.com/aqualinkd' target='_blank' rel='noopener'>GitHub</a></li>"+
-        "</ul>"+
+    "<nav class='mobile-nav show-on-mobiles'>" +
+    "<ul>" +
+    "<li" + pshome + "><a href='/'>Home</a></li>" +
+    "<li" + psoverview + "><a href='/overview/'>Overview</a></li>" +
+    "<li" + psdocs + "><a href='/docs/'>Docs</a></li>" +
+    "<li" + psdetails + "><a href='/details/'>Details</a></li>" +
+    "<li" + pspurchase + "><a href='/purchase/'>Purchase</a></li>" +
+    "<li><a href='https://github.com/aqualinkd' target='_blank' rel='noopener'>GitHub</a></li>" +
+    "</ul>" +
     "</nav>";
+} catch (e) { }
 
+try {
+  const footer = document.getElementById('footer');
 
-const footer = document.getElementById('footer');
-
-footer.innerHTML="<div class='grid'>"+
-      "<div class='unit whole center-on-mobiles''>"+
-      "<p>If you like this project, please consider donating <a href=' https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=SEGN9UNS38TXJ'"+
-        "rel='nofollow'><img "+
-          "src='https://camo.githubusercontent.com/2b3b3f38604d749b543e8577afdc6bd9fab25244f6cb16bfb713273a74350fd7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446f6e6174652d50617950616c2d677265656e2e737667'"+
-          "alt='Donate' data-canonical-src='https://img.shields.io/badge/Donate-PayPal-green.svg'"+
-          "style='max-width: 100%;'></a>"+
-        "</p>"+
-      "</div>"+
-      "<div class='unit two-thirds center-on-mobiles'>"+
-        "<p>AqualinkD is actively maintained in <a href='https://github.com/aqualinkd/'>core repos</a> free under the "+
-        "terms of the <a href='https://github.com/aqualinkd/AqualinkD/blob/master/LICENSE.md'>GPLv2 licensing</a>.</p>"+
-          "</div>"+
-      "<div class='unit one-third align-right center-on-mobiles'>"+
-        "<p>"+
-        "Hosted by <a href='https://github.com'> • GitHub</a>"+
-          "</p>"+
-        "</div>"+
-"</div>";
+  footer.innerHTML = "<div class='grid'>" +
+    "<div class='unit whole center-on-mobiles''>" +
+    "<p>If you like this project, please consider donating <a href=' https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=SEGN9UNS38TXJ'" +
+    "rel='nofollow'><img " +
+    "src='https://camo.githubusercontent.com/2b3b3f38604d749b543e8577afdc6bd9fab25244f6cb16bfb713273a74350fd7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446f6e6174652d50617950616c2d677265656e2e737667'" +
+    "alt='Donate' data-canonical-src='https://img.shields.io/badge/Donate-PayPal-green.svg'" +
+    "style='max-width: 100%;'></a>" +
+    "</p>" +
+    "</div>" +
+    "<div class='unit two-thirds center-on-mobiles'>" +
+    "<p>AqualinkD is actively maintained in <a href='https://github.com/aqualinkd/'>core repos</a> free under the " +
+    "terms of the <a href='https://github.com/aqualinkd/AqualinkD/blob/master/LICENSE.md'>GPLv2 licensing</a>.</p>" +
+    "</div>" +
+    "<div class='unit one-third align-right center-on-mobiles'>" +
+    "<p>" +
+    "Hosted by <a href='https://github.com'> • GitHub</a>" +
+    "</p>" +
+    "</div>" +
+    "</div>";
+} catch (e) { }
