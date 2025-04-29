8280b8116f387f7a44aa5e3283031cd8735c108c
sfeakes <sfeakes@gmail.com>
2025-04-29 11:22:47 -0500

    update

--- js/header.js
+++ js/header.js
@@ -111,5 +111,5 @@ try {
 const script = document.createElement('script');
 script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
 script.defer = true;
-script.setAttribute('data-cf-beason', '{"token": "b4dd62e89df4456f8df2e97ab636a41b"}');
+script.setAttribute('data-cf-beacon', '{"token": "b4dd62e89df4456f8df2e97ab636a41b"}');
 document.body.appendChild(script);
